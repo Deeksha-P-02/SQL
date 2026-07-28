eee40531fb0e3bdf09f6898313cea447e066aae3
Deeksha P <deekshaparthasarathi@gmail.com>
2026-07-28 21:33:21 +0530

    employee salary greater than 60000
    employee salary who  has greater than 60000

---
+++ emp salary greater than 60000.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE Employee (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES
+(101, 'John', 'HR', 45000),
+(102, 'Alice', 'IT', 60000),
+(103, 'David', 'Finance', 55000),
+(104, 'Emma', 'Marketing', 50000),
+(105, 'Robert', 'IT', 65000),
+(106, 'Sophia', 'HR', 70000);
+SELECT *FROM Employee
+WHERE Salary > 60000;
